21ccfc4bd6b956bea0cb9efbb77b03be33576367
linshuyi <lsy19961996@sina.cn>
2022-10-20 19:09:05 +0800

    加入render函数

--- src/creatElement/index.js
+++ src/creatElement/index.js
@@ -1,1 +1,18 @@
-function createElement(tag, props, children) {}
+function createElement(tag, props, children) {
+  return new Element(tag, props, children)
+}
+
+class Element {
+  constructor(tag, props, children) {
+    this.tag = tag
+    this.props = props
+    this.children = children
+  }
+}
+
+function render(vDom) {
+  
+}
+export {
+  createElement
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log(1)
+import { createElement } from './creatElement'
+
+// 创建虚拟dom
+const vDom = createElement('ul', { class: 'list' }, [
+  createElement('li', { class: 'item' }, ['周杰伦']),
+  createElement('li', { class: 'item' }, ['林俊杰']),
+  createElement('li',{class: 'item'},['王力宏'])
+])
+console.log(vDom)

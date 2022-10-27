a166ed06cfe694a4f49d0671a30af60531d83f74
linshuyi <lsy19961996@sina.cn>
2022-10-27 17:51:26 +0800

    console

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { createElement,render,renderDom } from './creatElement'
 import { domDiff } from './diff'
 import { doPatch } from './patch'
 // 创建虚拟dom
+// createElement相当于render函数
 const vDom1 = createElement('ul', { class: 'list',
   style: "width: 300px;height: 300px;background-color: orange"
 }, [
@@ -27,14 +28,14 @@ const vDom2 = createElement('ul', { class: 'list-wrap',
   [createElement('p',{class: 'text'},[])]),
   createElement('div',{class: 'item','data-index': 2 },['第三个列表项'])
 ])
+
+console.log(vDom1)// 虚拟节点
 // 虚拟dom-->真实dom
 const rdom = render(vDom1)
 // 获取补丁
 const p = domDiff(vDom1,vDom2)
 // 打补丁
 const patchDom = doPatch(rdom,p)
-console.log(patchDom)
-// 将虚拟dom渲染成真实dom
+// 将打过补丁的真实dom挂载到app下
 renderDom(patchDom, document.getElementById('app'))
-//domDiff
 
